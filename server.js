9abb95bee91b8623b20393047382f136837ac89c
Alex Mülli <amuelli@gmail.com>
2016-12-03 15:19:44 +0100

    fixed wrong env name

--- server.js
+++ server.js
@@ -122,12 +122,6 @@ server.listen(port, (err) => {
   if (err) {
     return console.error(err)
   }
-
-  console.log(`Listening on port ${port}`)
-  console.log(process.env.TWITTER_CONSUMER_KEY);
-  console.log(process.env.TWITTER_CONSUMER_SECRET);
-  console.log(process.env.TWITTER_ACCESS_TOKEN_KEY);
-  console.log(process.env.TWITTER_ACCESS_TOKEN_SECRET);
 })
 
 
@@ -135,7 +129,7 @@ var client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_CONSUMER_SECRET
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
 
