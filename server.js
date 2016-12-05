781b35fe7c9258d9bdd3020464758166317f6278
Alex Mülli <amuelli@gmail.com>
2016-12-05 23:37:08 +0100

    first working version

--- server.js
+++ server.js
@@ -17,26 +17,51 @@ var slapp = Slapp({
 })
 
 
-var HELP_TEXT = `
-I will respond to the following messages:
-\`help\` - to see this message.
-\`hi\` - to demonstrate a conversation that tracks state.
-\`thanks\` - to demonstrate a simple response.
-\`<type-any-other-text>\` - to demonstrate a random emoticon response, some of the time :wink:.
-\`attachment\` - to see a Slack attachment message.
-\`alarm\` - to see a Slack interactive message.
-`
-
 //*********************************************
 // Setup different handlers for messages
 //*********************************************
 
-// response to the user typing "help"
-slapp.message('help', ['mention', 'direct_message'], (msg) => {
-  msg.say(HELP_TEXT)
+
+slapp.event('message', (msg) => {
+  if( msg.body.event.subtype === 'channel_join' && msg.body.event.user === msg.body.authed_users[0]) {
+    msg.say('hoi zäme, ich bin der Velokontrolle-Bot und werde nun auf Twitter nach Hinweisen für Velokontrollen Ausschau halten.')
+    client.stream('statuses/filter', {track: 'velokontrolle'},  function(stream) {
+      stream.on('data', function(tweet) {
+        msg.say({
+          text: '<!here> Ich habe gerade diesen Tweet entdeckt: ' + ' https://twitter.com/' + tweet.user.screen_name + '/status/' + tweet.id_str
+        });
+        alarm(msg)
+      });
+      stream.on('error', function(error) {
+        console.log(error);
+      });
+    });
+  } else if(msg.body.event.text === 'Alarm') {
+    msg.say({
+      text: 'hani Alarm ghört?'
+    });
+    alarm(msg);
+  }
 })
 
-slapp.message('alarm', (msg) => {
+slapp.action('alarm_callback', 'alarm', (msg, value) => {
+  console.log('value', value)
+  if(value === 'alarm') {
+    msg.respond(msg.body.response_url, {
+      text: `Alarm wurde ausgelöst von <@${msg.body.user.id}>`,
+      delete_original: true
+    })
+    msg.say(`<!channel> ALARM!`)
+  } else if(value === 'noalarm') {
+    msg.respond(msg.body.response_url, {
+      text: `von <@${msg.body.user.id}> als Fehlalarm eingestuft`,
+      delete_original: true
+    })
+  }
+})
+
+
+function alarm(msg) {
   msg.say({
     text: 'Soll ein Velokontrolle-Alarm ausgelöst werden?',
     attachments: [
@@ -56,103 +81,18 @@ slapp.message('alarm', (msg) => {
               ok_text: 'Ja, Alarm!',
               dismiss_text: 'Nein'
             },
-            value: true
+            value: 'alarm'
           },
           {
             name: 'alarm',
             text: 'Nein, Fehlalarm',
             type: 'button',
-            value: false
+            value: 'noalarm'
           }
         ]
       }]
   })
-})
-
-slapp.action('alarm_callback', 'alarm', (msg, value) => {
-  if(value) {
-    msg.respond(msg.body.response_url, `ALARM!`)
-  }
-})
-
-// "Conversation" flow that tracks state - kicks off when user says hi, hello or hey
-slapp
-  .message('^(hi|hello|hey)$', ['direct_mention', 'direct_message'], (msg, text) => {
-    msg
-      .say(`${text}, how are you?`)
-      // sends next event from user to this route, passing along state
-      .route('how-are-you', { greeting: text })
-  })
-  .route('how-are-you', (msg, state) => {
-    var text = (msg.body.event && msg.body.event.text) || ''
-
-    // user may not have typed text as their next action, ask again and re-route
-    if (!text) {
-      return msg
-        .say("Whoops, I'm still waiting to hear how you're doing.")
-        .say('How are you?')
-        .route('how-are-you', state)
-    }
-
-    // add their response to state
-    state.status = text
-
-    msg
-      .say(`Ok then. What's your favorite color?`)
-      .route('color', state)
-  })
-  .route('color', (msg, state) => {
-    var text = (msg.body.event && msg.body.event.text) || ''
-
-    // user may not have typed text as their next action, ask again and re-route
-    if (!text) {
-      return msg
-        .say("I'm eagerly awaiting to hear your favorite color.")
-        .route('color', state)
-    }
-
-    // add their response to state
-    state.color = text
-
-    msg
-      .say('Thanks for sharing.')
-      .say(`Here's what you've told me so far: \`\`\`${JSON.stringify(state)}\`\`\``)
-    // At this point, since we don't route anywhere, the "conversation" is over
-  })
-
-// Can use a regex as well
-slapp.message(/^(thanks|thank you)/i, ['mention', 'direct_message'], (msg) => {
-  // You can provide a list of responses, and a random one will be chosen
-  // You can also include slack emoji in your responses
-  msg.say([
-    "You're welcome :smile:",
-    'You bet',
-    ':+1: Of course',
-    'Anytime :sun_with_face: :full_moon_with_face:'
-  ])
-})
-
-// demonstrate returning an attachment...
-slapp.message('attachment', ['mention', 'direct_message'], (msg) => {
-  msg.say({
-    text: 'Check out this amazing attachment! :confetti_ball: ',
-    attachments: [{
-      text: 'Slapp is a robust open source library that sits on top of the Slack APIs',
-      title: 'Slapp Library - Open Source',
-      image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
-      title_link: 'https://beepboophq.com/',
-      color: '#7CD197'
-    }]
-  })
-})
-
-// Catch-all for any other responses not handled above
-slapp.message('.*', ['direct_mention', 'direct_message'], (msg) => {
-  // respond only 40% of the time
-  if (Math.random() < 0.4) {
-    msg.say([':wave:', ':pray:', ':raised_hands:'])
-  }
-})
+}
 
 // attach Slapp to express server
 var server = slapp.attachToExpress(express())
